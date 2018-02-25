3f3697f796f0b596a8e88c1032d454bce1ff8b7f
DEEEEEEZNUTS <35668164+DEEEEEEZNUTS@users.noreply.github.com>
2018-02-25 20:47:07 +0100

    Add files via upload

--- Restore.sh
+++ Restore.sh
@@ -4,6 +4,5 @@ DB_BACKUP="backups/mysql_backup/`date +%Y-%m-%d`"
 SQLFILE=$DBNAME-${DATE}.sql
 cp -v $DB_BACKUP/$SQLFILE.gz .
 gunzip $DB_BACKUP/$SQLFILE.gz
-mysql --user=root --password=toor $DBNAME < /$DB_BACKUP/$SQLFILE
-rm -v $DB_BACKUP/*.sql;
-
+mysql --user=root --password=toor $DBNAME < $DB_BACKUP/$SQLFILE
+echo end;
